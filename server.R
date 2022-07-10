f5d375f7e62e484ebd81d6855ac6d0ccb099a0ff
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-07-10 12:58:49 -0400

    adding long lat

--- server.R
+++ server.R
@@ -1,4 +1,15 @@
 server <- function(input, output,session) {
+  output$lat <- renderText({
+    input$lat
+  })
+  
+  output$long <- renderText({
+    input$long
+  })
+  
+  output$geolocation <- renderText({
+    input$geolocation
+  })
     observeEvent(input$send,{
         print(shinybrowser::get_all_info()$device)
         print(input$lat)
